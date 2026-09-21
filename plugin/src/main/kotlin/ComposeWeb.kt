10ce20605c6263a5c6902c25254953ff79613bf0
Tanish Azad <tanishazad03@gmail.com>
2026-09-21 09:03:35 +0530

    format

--- plugin/src/main/kotlin/ComposeWeb.kt
+++ plugin/src/main/kotlin/ComposeWeb.kt
@@ -7,14 +7,23 @@ import kotlin.jvm.java
 
 abstract class ComposeWeb : Plugin<Project> {
     override fun apply(project: Project) {
-        val requiredPlugins = listOf("org.jetbrains.kotlin.multiplatform", "org.jetbrains.compose", "org.jetbrains.kotlin.plugin.compose")
-        if (!requiredPlugins.all { project.plugins.hasPlugin(it) }) {
-            throw IllegalStateException("ComposeWeb plugin requires the following plugins to be applied: $requiredPlugins")
+        val requiredPlugins = listOf(
+            "org.jetbrains.kotlin.multiplatform",
+            "org.jetbrains.kotlin.plugin.compose",
+            "org.jetbrains.compose"
+        )
+
+        check(requiredPlugins.all(project.plugins::hasPlugin)) {
+            "The following plugins must be applied: $requiredPlugins"
         }
 
         val buildDir = project.layout.buildDirectory.asFile.get()
 
-        project.tasks.register("runWebServer", RunWebServerTask::class.java, File(buildDir, "dist/wasmJs/productionExecutable")).configure {
+        project.tasks.register(
+            "runWebServer",
+            RunWebServerTask::class.java,
+            File(buildDir, "dist/wasmJs/productionExecutable")
+        ).configure {
             it.group = "Compose Web Navigator"
             it.description = "Runs a local web server to serve the Compose for Web application."
 
