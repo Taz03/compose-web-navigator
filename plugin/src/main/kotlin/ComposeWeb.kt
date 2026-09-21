de2697f20d84dc00dc5745436f45d244ac3f15fe
Tanish Azad <tanishazad03@gmail.com>
2026-09-21 05:57:29 +0530

    use wasmJsBrowserDistribution for compiling the project

--- plugin/src/main/kotlin/ComposeWeb.kt
+++ plugin/src/main/kotlin/ComposeWeb.kt
@@ -2,23 +2,22 @@ package io.github.taz03.compose.web.navigator
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import kotlin.jvm.java
 
 abstract class ComposeWeb : Plugin<Project> {
     override fun apply(project: Project) {
-        val buildDir = project.layout.buildDirectory.asFile.get()
-
-        project.tasks.register("buildWebBinaries", BuildBinariesTask::class.java, buildDir).configure {
-            it.group = "Compose Web Navigator"
-            it.description = "Builds the WebAssembly binaries and moves them to the app directory."
-
-            it.dependsOn("wasmJsBrowserProductionWebpack")
+        val requiredPlugins = listOf("org.jetbrains.kotlin.multiplatform", "org.jetbrains.compose", "org.jetbrains.kotlin.plugin.compose")
+        if (!requiredPlugins.all { project.plugins.hasPlugin(it) }) {
+            throw IllegalStateException("ComposeWeb plugin requires the following plugins to be applied: $requiredPlugins")
         }
+
+        val buildDir = project.layout.buildDirectory.asFile.get()
 
         project.tasks.register("runWebServer", RunWebServerTask::class.java, buildDir).configure {
             it.group = "Compose Web Navigator"
             it.description = "Runs a local web server to serve the Compose for Web application."
 
-            it.dependsOn("buildWebBinaries")
+            it.dependsOn("wasmJsBrowserDistribution")
         }
     }
 }
--- plugin/src/main/kotlin/RunWebServerTask.kt
+++ plugin/src/main/kotlin/RunWebServerTask.kt
@@ -11,9 +11,11 @@ import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
 import java.io.File
 import javax.inject.Inject
 
+@DisableCachingByDefault(because = "This task runs a local web server and does not produce any cacheable outputs.")
 abstract class RunWebServerTask @Inject constructor(
     private val buildDir: File
 ) : DefaultTask() {
@@ -22,13 +24,13 @@ abstract class RunWebServerTask @Inject constructor(
         embeddedServer(CIO, port = 8080) {
             install(StatusPages) {
                 status(HttpStatusCode.NotFound) { call, status ->
-                    val file = File(buildDir, "app/index.html")
+                    val file = File(buildDir, "dist/wasmJs/productionExecutable/index.html")
                     call.respondText(file.readText(), ContentType.Text.Html, status)
                 }
             }
 
             routing {
-                staticFiles("/", File(buildDir, "app")) {
+                staticFiles("/", File(buildDir, "dist/wasmJs/productionExecutable")) {
                     default("index.html")
                 }
             }
